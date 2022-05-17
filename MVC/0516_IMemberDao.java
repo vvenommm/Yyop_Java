e51c31d0d7d1b2ef66bd8b433a01c08af637eb00
vvenommm <96784333+vvenommm@users.noreply.github.com>
2022-05-17 13:45:22 +0900

    Create 0516_IMemberDao.java

---
+++ MVC/0516_IMemberDao.java
@@ -0,0 +1,71 @@
+package mvcDao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import mvcVo.MemberVO;
+
+/**
+ * 실제 DB와 연결해서 SQL문을 수행하여 결과를 작성.<br>
+ * Service에게 전달하는 DAO의 interface<br>
+ * 
+ * 메소드 하나가 DB와 관련된 작업 1개를 수행하도록 작성<br>
+ * @author Hoo
+ *
+ */
+
+public interface IMemberDao {
+	
+	/**
+	 * MemberVO 객체에 담겨진 자료를 DB에 insert하는 메소드
+	 * 
+	 * @param conn java.sql.Connetion 객체
+	 * @param memVo DB에 insert할 자료가 저장된 MemberVO객체
+	 * @return insert 작업 성공 : 1, 작업 실패 : 0
+	 * @throws SQLException
+	 */
+	
+	public int insertMember(Connection conn, MemberVO memVo) throws SQLException;
+	
+	/**
+	 * 회원 ID를 인수값으로 받아서 해당 회원 정보를 삭제하는 메도스
+	 * 
+	 * @param conn java.sql.Connetion 객체
+	 * @param memId memId 삭제할 회원 ID
+	 * @return 성공 : 1, 실패 : 0
+	 * @throws SQLException
+	 */
+	
+	public int deleteMember(Connection conn, String memId) throws SQLException;
+	
+	/**
+	 * MemberVO자료를 이용하여 회원 정보를 update하는 메소드
+	 * 
+	 * @param conn java.sql.Connetion 객체
+	 * @param memVo update할 회원 정보가 저장된 MemberVO객체
+	 * @return 작업 성공 : 1, 작업 실패 : 0
+	 * @throws SQLException
+	 */
+	public int updateMember(Connection conn, MemberVO memVo) throws SQLException;
+	
+	/**
+	 * DB 전체 회원 정보를 가져와서 List에 담아 반환하는 메소드
+	 * 
+	 * @param conn java.sql.Connetion 객체
+	 * @return MemberVO객체가 저장된 List
+	 * @throws SQLException
+	 */
+	public List<MemberVO> getAllMember(Connection conn) throws SQLException;
+	
+	/**
+	 * 회원 ID를 인수값으로 받아서 해당 회원 ID의 개수를 반환하는 메소드
+	 * 
+	 * @param conn java.sql.Connetion 객체
+	 * @param memId 검색할 회원 ID
+	 * @return 검색된 회원 ID 개수
+	 * @throws SQLException
+	 */
+	public int getMemberCount(Connection conn, String memId) throws SQLException;
+	
+}
