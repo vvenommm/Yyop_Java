a575f08895bb7021227ab95388a788379d19ac6a
vvenommm <96784333+vvenommm@users.noreply.github.com>
2022-05-26 17:14:52 +0900

    Create 0526_RequestTest02.java

---
+++ Servlet/0526_RequestTest02.java
@@ -0,0 +1,79 @@
+package kr.or.ddit.basic.reqNresp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RequestTest02
+ */
+@WebServlet("/requestTest02.do")
+public class RequestTest02 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setCharacterEncoding("utf-8");
+		
+		int no1 = Integer.parseInt(request.getParameter("no1"));
+		int no2 = Integer.parseInt(request.getParameter("no2"));
+		String op = request.getParameter("op");
+		double result = 0; //계산 결과 값
+		
+		switch(op) {
+		case "+": result = no1 + no2; break;
+		case "-": result = no1 - no2; break;
+		case "*": result = no1 * no2; break;
+		case "/": result = no1 / no2; break;
+		case "%": result = no1 % no2; break;
+		}
+		
+//		String minus = request.getParameter("minus");
+//		String multi = request.getParameter("multi");
+//		String div = request.getParameter("div");
+//		String mod = request.getParameter("mod");
+		
+		
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("<html><head><meta charset='utf-8'>");
+		out.println("<title>Request 객체 연습용2</title></head>");
+		out.println("<body>");
+		out.println("<h2>RequestTest02.java</h2>");
+		out.println("<h3>계산 결과</h3>");
+		out.println("<hr>");
+		out.println("<p>" + no1 + " " + op + " " + no2 + " = " + result + "</p>");
+		out.printf("%d %s %d = %f<br>", no1, op, no2, result);
+//		if(op.equals("+")) {
+//			out.println((no1 + no2) + "</p>");
+//		}else if(op.equals("-")) {
+//			out.println((no1 - no2) + "</p>");
+//		}else if(op.equals("*")) {
+//			out.println((no1 * no2) + "</p>");
+//		}else if(op.equals("/")) {
+//			out.println(((double)no1 / (double)no2) + "</p>");
+//		}else if(op.equals("%")) {
+//			out.println((no1 % no2) + "</p>");
+//		}
+		
+		out.println("</body></html>");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
