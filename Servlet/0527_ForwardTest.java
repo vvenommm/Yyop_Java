bcfd3a3495c93b8207f5faf280a3acf96e7291ff
vvenommm <96784333+vvenommm@users.noreply.github.com>
2022-05-27 14:22:18 +0900

    Update 0527_ForwardTest.java

--- Servlet/0527_ForwardTest.java
+++ Servlet/0527_ForwardTest.java
@@ -1,1 +1,53 @@
+package kr.or.ddit.basic.reqNresp;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ForwardTest
+ */
+@WebServlet("/forwardTest.do")
+public class ForwardTest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setCharacterEncoding("utf-8");
+		
+		String userName = request.getParameter("username"); //파라미터 값 구하기
+		
+		//이전 문서에서 setAttribute()로 보낸 데이터는 getAttribute() 메소드로 수신
+		//형식) request객체.getAttribute("키값");
+		
+		String tel = (String)request.getAttribute("tel");
+		
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		
+		out.println("<html><head><meta charset='utf-8'>");
+		out.println("<title>Forward방식 연습</title></head>");
+		out.println("<body>");
+		out.println("<h3>Forward 방식으로 넘어온 데이터</h3><hr>");
+		out.println("이 름 : " + userName + "<br>");
+		out.println("전화번호 : " + tel + "<br>");
+		out.println("</body></html>");
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
