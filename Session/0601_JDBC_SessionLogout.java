2ab1fef5e3b34ca6dbfbaf6d59fc5758ccf81add
vvenommm <96784333+vvenommm@users.noreply.github.com>
2022-06-01 19:57:50 +0900

    Create 0601_JDBC_SessionLogout.java

---
+++ Session/0601_JDBC_SessionLogout.java
@@ -0,0 +1,31 @@
+package kr.or.ddit.basic.session;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class SessionLogin
+ */
+@WebServlet("/JDBC_SessionLogout.do")
+public class JDBC_SessionLogout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		
+		session.invalidate();
+		response.sendRedirect(request.getContextPath()+ "/basic/session/jdbc_sessionLogin.jsp");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
