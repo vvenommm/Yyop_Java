dcea3c510803a219cb27ffcc2754d3de6b50a0ab
vvenommm <96784333+vvenommm@users.noreply.github.com>
2022-06-02 14:10:17 +0900

    Update and rename 0601_JDBC_ISessionService.java to 0601_iBatis_ISessionService.java

--- Session/0601_JDBC_ISessionService.java
+++
@@ -1,7 +0,0 @@
-package kr.or.ddit.basic.session;
-
-public interface JDBC_ISessionService {
-	
-	public JDBC_MemberVO getOneMember(String mem_id);
-
-}
---
+++ Session/0601_iBatis_ISessionService.java
@@ -0,0 +1,7 @@
+package kr.or.ddit.basic.session;
+
+public interface iBatis_ISessionService {
+	
+	public iBatis_MemberVO getOneMember(iBatis_MemberVO loginVo);
+
+}
