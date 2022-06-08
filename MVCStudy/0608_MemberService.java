1980c8ea640893d67825711ad55b20d5df7ce616
vvenommm <96784333+vvenommm@users.noreply.github.com>
2022-06-08 21:23:53 +0900

    Create 0608_MemberService.java

---
+++ MVCStudy/0608_MemberService.java
@@ -0,0 +1,11 @@
+package kr.or.ddit.member.service;
+
+import java.util.List;
+
+import kr.or.ddit.vo.MemberVO;
+
+public interface MemberService {
+
+	public List<MemberVO> getAllMemberList();
+
+}
